1a29c22e96b31fcbd6f7d171c8c343f60200d56e
Alva Chien <alvachien@live.com>
2017-12-02 15:48:54 +0800

    Create the project

---
+++ enwordfetcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace enwordfetcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
